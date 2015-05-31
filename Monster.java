fd6b34a756e677f25bcdcea2395793348e94dff3
dkiang <dkiang@punahou.edu>
2015-05-31 06:47:07 -1000

    Added debug code
    Monsters and NPCs now report their remaining health. Notice that
    attacks deal damage to NPC but monster remains unaffected.

--- Monster.java
+++ Monster.java
@@ -2,14 +2,21 @@ public class Monster extends NPC
 {
   int myHealth;
   String name = "";
+  int myCol; //values keep track of where monster is on the matrix
+  int myRow;
   boolean onFire = false;
   boolean poisoned = false;
   boolean immobilized = false;
-  public Monster(int count, String monsterName)
+  public Monster(int count, String monsterName, int monsterCol, int row)
   {
-    myHealth = (super.d20())*count;
+    myHealth = (super.d20() * count);
     name = monsterName;
+    myCol = monsterCol;
+    myRow = row;
   }
+  
+  
+  
   public void randomAttack(Player player, int count) //determines randomly which attack the monster will use
   {
     int temp = (int)(Math.random() * 4 + 1);
@@ -35,8 +42,9 @@ public class Monster extends NPC
     int damage;
     if (super.d20() > 5)
     {
-      damage = super.d4() * count;
+      damage = super.d4() + count;
       System.out.println("The " + name + " attacks you for " + damage + " damage.");
+      System.out.println("The " + name + " has " + myHealth + " health."); // Mr. K. debug code
       player.takeDamage(damage);
     }
     else
@@ -51,8 +59,9 @@ public class Monster extends NPC
     int damage;
     if (super.d20() > 8)
     {
-      damage = super.d6() * count;
+      damage = super.d6() + count;
       System.out.println("The " + name + " attacks you for " + damage + " damage.");
+      System.out.println("The " + name + " has " + myHealth + " health."); // Mr. K. debug code
       player.takeDamage(damage);
     }
     else
@@ -67,8 +76,9 @@ public class Monster extends NPC
     int damage;
     if (super.d20() > 10)
     {
-      damage = super.d8() * count;
+      damage = super.d8() + count;
       System.out.println("The " + name + " attacks you for " + damage + " damage.");
+      System.out.println("The " + name + " has " + myHealth + " health."); // Mr. K. debug code
       player.takeDamage(damage);
     }
     else
@@ -83,8 +93,9 @@ public class Monster extends NPC
     int damage;
     if (super.d20() > 15)
     {
-      damage = super.d8() * count;
+      damage = super.d8() + count;
       System.out.println("The " + name + " attacks you for " + damage + " damage.");
+      System.out.println("The " + name + " has " + myHealth + " health."); // Mr. K. debug code
       player.takeDamage(damage);
     }
     else
@@ -94,7 +105,7 @@ public class Monster extends NPC
     }
   }
   
-  public void takeDamage(int damage) //methods to lower class variables
+  public void takeDamage(int damage, String turn) //methods to lower class variables
   {
     myHealth -= damage;
     System.out.println("The " + name + " has " + myHealth + " health.");
